663af85423475eec71f117951c8cd77320299245
unknown <vidhyakhadatatre@gmail.com>
2025-04-20 14:50:27 +0530

    added prime number program

---
+++ src/test/java/javapractices/PrimeNumber.java
@@ -0,0 +1,60 @@
+package javapractices;
+
+import java.util.Scanner;
+
+
+
+public class PrimeNumber { 
+
+	public static void main(String[] args) 
+	
+	
+	{
+		
+		Scanner scanner = new Scanner(System.in);
+				
+		System.out.println("please enter number to check Prime number...");
+
+		int num=Integer.parseInt(scanner.next());
+		
+		boolean prime= false;
+		for(int i=2;i<num/2;i++)
+		{
+	
+	if(num% i == 0) {
+		prime = false;
+		
+	}
+	if (num % i != 0 ) {
+		
+		prime = true;
+		
+		break;
+	}
+	if (num % i !=0) {
+		prime= true;
+		
+	}
+		}
+		
+		if(prime)
+			
+			System.out.println("given number is prime ");
+		else
+		System.out.println("given number is not a prime");
+		
+	}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+	
