241a4ea5b5570837e4fc871ce9784889f1b889e7
unknown <vidhyakhadatatre@gmail.com>
2025-04-21 15:00:49 +0530

    changes in table print file

--- src/test/java/javapractices/TablePrint.java
+++ src/test/java/javapractices/TablePrint.java
@@ -1,15 +1,17 @@
 package javapractices;
 
-public class TablePrint {
+public class TablePrint 
+{
 
 	public static void main(String[] args) {
 		
-	for( int i = 2; i <= 200;i++)
-	{
+	for( int i = 2; i <= 200;i++){
 		for(int j = 1;j <= 10;j++) {
 		System.out.println(i * j  + " ");
 		
 		}
+		
+		
 		System.out.println();
 		
 		
